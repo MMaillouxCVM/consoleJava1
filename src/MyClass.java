187ef4b4e67aa1b451def39100ed4281556ea00c
Mathieu Mailloux <mmailloux@cvm.qc.ca>
2025-04-01 07:43:03 -0400

    MyClass.java added

---
+++ src/MyClass.java
@@ -0,0 +1,25 @@
+public class MyClass {
+    private int entier1;
+    private int entier2;
+
+    public MyClass(int entier1, int entier2) {
+        this.entier1 = entier1;
+        this.entier2 = entier2;
+    }
+
+    public int getEntier1() {
+        return entier1;
+    }
+
+    public void setEntier1(int entier1) {
+        this.entier1 = entier1;
+    }
+
+    public int getEntier2() {
+        return entier2;
+    }
+
+    public void setEntier2(int entier2) {
+        this.entier2 = entier2;
+    }
+}
