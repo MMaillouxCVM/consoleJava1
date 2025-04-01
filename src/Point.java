71f5ce6dc05907bf90ad12d3e03d08ba6df90f00
Mathieu Mailloux <mmailloux@cvm.qc.ca>
2025-04-01 09:19:30 -0400

    Segment: isCrossing() Point: produitVectoriel(), orientation()

--- src/Point.java
+++ src/Point.java
@@ -22,4 +22,18 @@ public class Point {
     public void setY(float y) {
         this.y = y;
     }
+
+    public float produitVectoriel(Point pt1, Point pt2) {
+        return (pt2.getX() - pt1.getX())*(this.y-pt1.getY())-(pt2.getY()-pt1.getY())*(this.x-pt1.getX());
+    }
+
+    public int orientation(Point pt1, Point pt2){
+        if (produitVectoriel(pt1, pt2) > 0) {
+            return 1;
+        } else if (produitVectoriel(pt1, pt2) < 0) {
+            return -1;
+        } else {
+            return 0;
+        }
+    }
 }
--- src/Segment.java
+++ src/Segment.java
@@ -24,10 +24,22 @@ public class Segment {
     }
 
     public boolean isCrossing(Segment s) {
-       return true;
+       int o1 = pt1.orientation(s.getPt1(), s.getPt2());
+       int o2 = pt2.orientation(s.getPt1(), s.getPt2());
+       int o3 = s.getPt1().orientation(pt1, pt2);
+       int o4 = s.getPt2().orientation(pt1, pt2);
+       if (o1 != o2 && o3 != o4) {
+           return true;
+       }
+       return false;
     }
 
     public boolean isColinear(Segment s) {
+        int o1 = pt1.orientation(s.getPt1(), s.getPt2());
+        int o2 = pt2.orientation(s.getPt1(), s.getPt2());
+        int o3 = s.getPt1().orientation(pt1, pt2);
+        int o4 = s.getPt2().orientation(pt1, pt2);
+
         return true;
     }
 }
